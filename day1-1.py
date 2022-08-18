fd9ff02acb01b25ec9dab637417d13ae656d5b51
Sarah Lindner <sa_lindner@posteo.at>
2022-08-18 18:18:43 +0200

    sum over 3 consecutive input values works

--- day1-1.py
+++ day1-1.py
@@ -10,8 +10,13 @@ data.columns = ['input']
 # calculate difference between two consecutive input values in 'input' column
 # and write result into new column named 'difference'
 data['difference'] = data['input'].diff()
-print(data)
+# print(data)
 
 # count values where the calculated difference > 0=
 # i.e. consecutive input values increased
 print(f"Number of increases: {len(data[data['difference']>0])}")
+
+for input in data['input']:
+    data['sum_over_three'] = data['input'].shift(1) + data['input'].shift(2) + data['input'].shift(3)    
+
+print(data)
