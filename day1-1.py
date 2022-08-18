fce29e50d1e36ad4416cd66b0b845da2582384d3
Sarah Lindner <sa_lindner@posteo.at>
2022-08-18 10:58:54 +0200

    program returns correct value of increases

--- day1-1.py
+++ day1-1.py
@@ -7,23 +7,11 @@ data = pd.read_csv('input.txt',delimiter="\t", header = None)
 data.columns = ['input']
 # print(data)
 
-# calculate difference between two consecutive values in 'input' column
+# calculate difference between two consecutive input values in 'input' column
 # and write result into new column named 'difference'
 data['difference'] = data['input'].diff()
 print(data)
 
-
-
-#create DataFrame
-df2 = pd.DataFrame({'period': [1, 2, 3, 4, 5, 6, 7, 8],
-                   'sales': [12, 14, 15, 13, 18, 20, 19, 24],
-                   'returns': [2, 2, 3, 3, 5, 4, 4, 6]})
-
-#find difference between each current row and the previous row
-df2['sales_diff'] = df2['sales'].diff()
-
-#filter for rows where difference is less than zero
-# df2 = df2[df2['sales_diff']<0]
-
-#view DataFrame
-print(df2)
+# count values where the calculated difference > 0=
+# i.e. consecutive input values increased
+print(f"Number of increases: {len(data[data['difference']>0])}")
