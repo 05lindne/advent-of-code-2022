baa16d20006efafdd4d4ec7fa583ec56613ef02c
Sarah Lindner <sa_lindner@posteo.at>
2022-08-20 15:10:29 +0200

    day2 produce dataframe from input

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,8 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('input.txt', delimiter="\t", header = None)
+df = pd.read_csv('input.txt', delimiter=" ", header = None)
+
+df.columns = ['direction', 'steps']
+
+print(df)
